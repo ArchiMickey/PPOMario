0d8f0121f5e68d17cc3e0e1f57783f67bebbcacd
ArchiMickey <wuchunyat119@gmail.com>
2022-07-15 02:38:42 +0800

    add dir autocreate

--- log.py
+++ log.py
@@ -1,12 +1,14 @@
 from typing import List
 import wandb
 from moviepy.editor import *
+from pathlib import Path
 
 
 def log_video(env_name: str, frames: List, durations: List, curr_steps: int, episode_reward: int, fps: int):
+    Path(f"train_video/{env_name}").mkdir(parents=True, exist_ok=True)
     clip = ImageSequenceClip(frames, durations=durations)
-    clip.write_videofile(f"train_video/{env_name}/mario_step{curr_steps}_reward{episode_reward}.mp4",
+    clip.write_videofile(f"train_video/{env_name}/mario_step{curr_steps}_reward{episode_reward:.3f}.mp4",
                             fps=fps,
                             )
-    # wandb.log({f"gameplay": wandb.Video(f"train_video/{env_name}/mario_step{curr_steps}_reward{episode_reward}.mp4",
-    #                                     caption=f"reward: {episode_reward}")})
+    wandb.log({f"gameplay": wandb.Video(f"train_video/{env_name}/mario_step{curr_steps}_reward{episode_reward:.3f}.mp4",
+                                        caption=f"reward: {episode_reward:.3f}")})
