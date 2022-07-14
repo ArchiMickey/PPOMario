d3ad1ca8f93fa98d5824bfedb8ec1f883083d865
ArchiMickey <wuchunyat119@gmail.com>
2022-07-15 02:39:02 +0800

    add multienv

--- multienv.py
+++ multienv.py
@@ -1,15 +1,13 @@
-from re import L
-from typing import Callable, List
+from typing import List
 import numpy as np
 import ray
-from torch import nn
 from wandb import agent
 from env import make_mario
 
 from icecream import ic
 
 @ray.remote
-class Remote_env:
+class RemoteAgent:
     def __init__(self, world:int, stage:int) -> None:
         self.env = make_mario(world, stage)
     
@@ -21,27 +19,45 @@ class Remote_env:
         return (next_state, reward, done, info)
 
 class MultiEnv:
-    def __init__(self, num_workers: int = 1) -> None:
+    def __init__(self, world:int = 1, stage: int = 1, num_workers: int = 1) -> None:
         self.num_workers = num_workers
-        self.envs = [Remote_env.remote(1, 1) for _ in range(num_workers)]
-        ic(self.envs)
+        self.envs = [RemoteAgent.remote(world, stage) for _ in range(num_workers)]
     
-    def reset(self):
+    def reset_all(self):
+        states = []
         for i in range(self.num_workers):
-            self.envs[i].reset.remote()
+            states.append(ray.get(self.envs[i].reset.remote()))
+        return np.array(states)
     
-    def step(self, action: List[int]):
-        rt = [[] for _ in range(self.num_workers)]
-        for i in range(self.num_workers):
-            next_state, reward, done, info = ray.get(self.envs[i].step.remote(np.random.randint(0, 7)))
-            exp = (next_state, reward, done, info)
-            rt[i] = exp
-        return rt
+    def step(self, action: List[int] or int):
+        next_states = []
+        rewards = []
+        dones = []
+        infos = []
+        if isinstance(action, int):
+                next_state, reward, done, info = ray.get(self.envs[0].step.remote(action))
+                next_states.append(next_state)
+                rewards.append(reward)
+                dones.append(done)
+                infos.append(info)
+        else:
+            for i in range(self.num_workers):
+                next_state, reward, done, info = ray.get(self.envs[i].step.remote(action[i]))
+                next_states.append(next_state)
+                rewards.append(reward)
+                dones.append(done)
+                infos.append(info)
+        for index, done in enumerate(dones):
+            if done:
+                self.envs[index].reset.remote()
+        return (np.array(next_states), np.array(rewards), np.array(dones), infos)
 
 def main(num_workers: int = 1) -> None:
     env = MultiEnv(num_workers)
-    env.reset()
-    ic(env.step([]))
+    states = env.reset_all()
+    ic(states.shape)
+    next_states, rewards, dones, infos = env.step([])
+    ic(next_states.shape)
     
 if __name__ == "__main__":
-    main(4)
+    main(32)
