4e7320e6d0c6a60b969b7f8c33334e7452210cab
ArchiMickey <wuchunyat119@gmail.com>
2022-07-22 01:03:04 +0800

    better organised code

--- train.py
+++ train.py
@@ -12,37 +12,43 @@ checkpoint_callback = ModelCheckpoint(
     mode="max",
     dirpath="model/",
     filename="ppomario-{test_score:.2f}-{step}",
-    every_n_train_steps=1000,
+    every_n_train_steps=5000,
     save_last=True,
 )
 
-# 1 training step = 2.5 global step
-model = PPO(
-    world=1,
-    stage=1,
-    nb_optim_iters=1,
-    batch_epoch=10,
-    batch_size=32,
-    num_workers=6,
-    hidden_size=512,
-    steps_per_epoch=512,
-    render_freq=10000,
-)
+def main(world: int = 1, stage: int = 1, ckpt_path: str = None):
+    if ckpt_path is not None:
+        model = PPO.load_from_checkpoint(ckpt_path)
+    
+    else:
+        model = PPO(
+            world=world,
+            stage=stage,
+            lr=1e-3,
+            nb_optim_iters=1,
+            batch_epoch=10,
+            batch_size=64,
+            num_workers=6,
+            hidden_size=512,
+            steps_per_epoch=1024,
+            render_freq=10000,
+        )
 
-now_dt = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-wandb_logger = WandbLogger(name=f"PPOMario-{now_dt}")
+    wandb_logger = WandbLogger(name=f"PPOMario-{world}-{stage}")
 
-trainer = pl.Trainer(
-    accelerator="gpu",
-    devices = 1 if torch.cuda.is_available() else None,
-    max_steps=2000000,
-    logger=wandb_logger,
-    log_every_n_steps=10,
-    default_root_dir="model",
-    gradient_clip_val= 100.0,
-    auto_lr_find=True,
-    callbacks=[checkpoint_callback, LearningRateMonitor(logging_interval='step')],
-)
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices = 1 if torch.cuda.is_available() else None,
+        max_steps=2000000,
+        logger=wandb_logger,
+        log_every_n_steps=10,
+        default_root_dir="model",
+        gradient_clip_val= 100.0,
+        auto_lr_find=True,
+        callbacks=[checkpoint_callback, LearningRateMonitor(logging_interval='epoch')],
+    )
+    
+    trainer.fit(model)
 
-# trainer.tune(model)
-trainer.fit(model)
+if __name__ == "__main__":
+    main(world=2, stage=1)
