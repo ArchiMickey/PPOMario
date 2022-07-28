8f16e1b7988883e187c9117ed96c46fb6e394972
ArchiMickey <wuchunyat119@gmail.com>
2022-07-29 00:13:07 +0800

    update neural network

--- src/network.py
+++ src/network.py
@@ -19,18 +19,15 @@ class ActorCriticCNN(nn.Module):
             nn.ReLU(),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Flatten(),
         )
-        
-        conv_out = self._get_conv_out(state_dim)
-        
+                
         self.actor_head = nn.Sequential(
-            nn.Linear(conv_out, hidden_dim),
+            nn.Linear(1152, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, num_actions),
         )
         self.critic_head = nn.Sequential(
-            nn.Linear(conv_out, hidden_dim),
+            nn.Linear(1152, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, 1),
         )
@@ -43,16 +40,6 @@ class ActorCriticCNN(nn.Module):
                 # nn.init.xavier_uniform_(module.weight)
                 # nn.init.kaiming_uniform_(module.weight)
                 nn.init.constant_(module.bias, 0)
-    
-    def _get_conv_out(self, shape) -> int:
-        """Calculates the output size of the last conv layer.
-        Args:
-            shape: input dimensions
-        Returns:
-            size of the conv output
-        """
-        conv_out = self.conv(torch.zeros(1, *shape))
-        return int(np.prod(conv_out.size()))
     
     def forward(self, x) -> Tuple[Tensor, Tensor]:
         """Forward pass through network
