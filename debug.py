410137b420dde30aadb44662b5627f8ffa2f9250
ArchiMickey <wuchunyat119@gmail.com>
2022-08-02 13:29:34 +0800

    add debug script

--- debug.py
+++ debug.py
@@ -1,3 +1,6 @@
+from icecream import install
+install()
+
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
@@ -11,7 +14,7 @@ def main(world: int = 1, stage: int = 1, ckpt_path: str = None):
         save_top_k=3,
         monitor="avg_score",
         mode="max",
-        dirpath="model/",
+        dirpath=f"model/{world}-{stage}/",
         filename="ppomario-{avg_score:.2f}",
         every_n_train_steps=5000,
         save_last=True,
@@ -20,17 +23,17 @@ def main(world: int = 1, stage: int = 1, ckpt_path: str = None):
     model = PPOMario(
         world=world,
         stage=stage,
-        lr=1e-4,
-        lr_decay_ratio=0.1,
-        lr_decay_epoch=15000,
+        lr=2.5e-4,
+        lr_decay_ratio=0,
+        lr_decay_epoch=100000,
         nb_optim_iters=1,
-        batch_epoch=10,
-        batch_size=16,
-        num_workers=6,
+        batch_epoch=12,
+        batch_size=4,
+        num_workers=1,
         hidden_size=512,
-        steps_per_epoch=128,
-        val_episodes=5,
-        val_interval=10000,
+        steps_per_epoch=16,
+        val_episodes=1,
+        render=False,
     )
 
     wandb_logger = WandbLogger(name=f"PPOMario-{world}-{stage}", offline=True)
@@ -38,9 +41,10 @@ def main(world: int = 1, stage: int = 1, ckpt_path: str = None):
     trainer = pl.Trainer(
         accelerator="gpu",
         devices = 1 if torch.cuda.is_available() else None,
-        max_steps=2000000,
+        max_epochs=100000,
         logger=wandb_logger,
         default_root_dir=f"model/{world}-{stage}",
+        check_val_every_n_epoch=10,
         gradient_clip_val= 100,
         auto_lr_find=True,
         callbacks=[checkpoint_callback, LearningRateMonitor(logging_interval='epoch'), ModelSummary(max_depth=5)],
