9a3b7b02c00a11f4f25006ec888f678e3c199af1
ArchiMickey <wuchunyat119@gmail.com>
2022-08-12 13:15:53 +0800

    add lstm ppo model

--- src/models.py
+++ src/models.py
@@ -49,6 +49,64 @@ class PPO(nn.Module):
     def forward(self, x):
         conv_out = self.conv(x)
         return self.action_head(conv_out), self.critic_head(conv_out)
+    
+class LSTMPPO(nn.Module):
+    def __init__(self, state_dim, hidden_dim, num_actions) -> None:
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=state_dim[0], out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        
+        conv_out_size = self._get_conv_out(state_dim)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, hidden_dim),
+            nn.ReLU(),
+        )
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim / 4)
+        
+        self.action_head = nn.Sequential(
+            nn.Linear(hidden_dim / 4, num_actions),
+            nn.Softmax(dim=-1),
+        )
+        self.critic_head = nn.Sequential(
+            nn.Linear(hidden_dim / 4, 1),
+        )
+        self.apply(init_)
+    
+    def _get_conv_out(self, shape):
+        conv_out = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(conv_out.size()))
+    
+    def get_states(self, x, lstm_state, done):
+        conv_out = self.conv(x)
+        
+        batch_size = x.size(0)
+        hidden = conv_out.reshape((-1, batch_size, self.lstm.input_size))
+        done = done.reshape((-1, batch_size))
+        new_hidden = []
+        for h, d in zip(hidden, done):
+            h, lstm_state = self.lstm(
+                h.unsqueeze(0),
+                {
+                    (1.0 - d).view(1, -1, 1) * lstm_state[0],
+                    (1.0 - d).view(1, -1, 1) * lstm_state[1],
+                }
+            )
+            new_hidden += [h]
+        new_hidden = torch.flatten(torch.cat(new_hidden), 0, 1)
+        return new_hidden, lstm_state
+    
+    def forward(self, x, lstm_state, done,):
+        hidden, lstm_state = self.get_states(x, lstm_state, done)
+        return self.action_head(hidden), self.critic_head(hidden), lstm_state
 
 class PPG(nn.Module):
     def __init__(self, state_dim, hidden_dim, num_actions) -> None:
